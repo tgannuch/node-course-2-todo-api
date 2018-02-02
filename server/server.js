b9dc6ac15e86dc864261571846d882bfa0cf3072
Tony Gannuch <tgannuch@gmail.com>
2018-02-02 15:39:19 -0600

    add todos refactor, refactor

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, resp) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+	todo.save().then((doc) => {
+		resp.send(doc);
+	}, (e) => {
+		resp.status(400).send(e);
+	});
+});
+
+app.listen(3000, () => {
+	console.log('server started on port 3000');
+});
+
