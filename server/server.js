ab104dbbf4979b7768182c609e290b34e97e246f
Tony Gannuch <tgannuch@gmail.com>
2018-02-02 16:38:53 -0600

    added get /todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.listen(3000, () => {
 	console.log('server started on port 3000');
 });
 
+app.get('/todos', (req, resp) => {
+	Todo.find().then(todos => {
+		resp.send({todos});
+	}, (err) => {
+		if (err) {
+			resp.status(400).send(err);
+		}
+	});
+});
 
 
 module.exports = {
