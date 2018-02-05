5ce2089e505a373d2105c2f73d0750475bbef3d1
Tony Gannuch <tgannuch@gmail.com>
2018-02-05 13:53:09 -0600

    hash user password on save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-}
+// var data = {
+// 	id: 10
+// }
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var password = '123bc!';
+
+// var hash = bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+var hashedPassord = '$2a$10$xax94SwcTPWXrpQZA54ZIO3s/jeFZTMVU.P.DsLivKoWBSmjWALDK';
+
+bcrypt.compare(password, hashedPassord, (err, res) => {
+	console.log(res);
+});
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // var message = 'I am user number 3';
