3c1acb03e73d5195e181521721baee771dd89659
Tony Gannuch <tgannuch@gmail.com>
2018-02-05 16:58:37 -0600

    added test for POST /usres/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -159,6 +159,8 @@ describe('POST /users', () => {
 					expect(user).toExist();
 					expect(user.password).toNotBe(password);
 					done();
+				}).catch((err) => {
+					done(err);
 				});
 			});
 	});
@@ -182,3 +184,54 @@ describe('POST /users', () => {
 			.end(done);
 	});
 });
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token',  (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: usersSeed[1].email,
+				password: usersSeed[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toExist();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(usersSeed[1]._id).then((user) => {
+					expect(user.tokens[0]).toInclude({
+						access: 'auth',
+						token: res.headers['x-auth']
+					});
+					done();
+				}).catch((err) => {
+					done(err);
+				});
+			});
+	});
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: usersSeed[1].email,
+				password: '123'
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toNotExist();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(usersSeed[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((err) => {
+					done(err);
+				});
+			});
+	});
